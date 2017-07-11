d5ee89c58e13168dc254793d8871ed6a7e77009a
yuandengfeng <yuandengfeng123@163.com>
2017-07-11 15:58:51 +0800

    json toMap  toList

--- src/main/java/com/rest/JsonTest.java
+++ src/main/java/com/rest/JsonTest.java
@@ -4,17 +4,51 @@ package com.rest;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
+import java.util.*;
+
 
 /**
  * Created by Administrator on 2016/8/2.
  */
 public class JsonTest {
+    public static HashMap<String, Object> toMap(JSONObject object) throws Exception {
+        HashMap<String, Object> map = new HashMap<String, Object>();
 
+        Iterator<String> keysItr = object.keys();
+        while(keysItr.hasNext()) {
+            String key = keysItr.next();
+            Object value = object.get(key);
 
-    public static void main(String[] args)
-    {
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            map.put(key, value);
+        }
+        return map;
+    }
+    public static List<Object> toList(JSONArray array) throws Exception {
+        List<Object> list = new ArrayList<Object>();
+        for(int i = 0; i < array.size(); i++) {
+            Object value = array.get(i);
+            if(value instanceof JSONArray) {
+                value = toList((JSONArray) value);
+            }
+
+            else if(value instanceof JSONObject) {
+                value = toMap((JSONObject) value);
+            }
+            list.add(value);
+        }
+        return list;
+    }
+    public static JSONObject jsonTest(){
         JSONObject root = new JSONObject();
         root.put("cat","it");
+        root.put("dog","he");
         JSONObject lan1 = new JSONObject();
         lan1.put("id",1);
         lan1.put("ide","eclipse");
@@ -33,13 +67,36 @@ public class JsonTest {
         array.add(2,lan3);
         root.put("lan",array);
 
-        System.out.println(root.toString());
+//        System.out.println(root.toString());
 
-        JSONArray array1 = new JSONArray();
+//        JSONArray array1 = new JSONArray();
+//
+//        array1 = root.getJSONArray("lan");
+//        System.out.println(array1.toString());
+//        System.out.println(System.currentTimeMillis());
+        return root;
+    }
 
-        array1 = root.getJSONArray("lan");
-        System.out.println(array1.toString());
-        System.out.println(System.currentTimeMillis());
+
+    public static void main(String[] args)
+    {
+      JSONObject jsonObject =jsonTest();
+        try {
+            Map<String, Object> map =toMap(jsonObject);
+            for(Map.Entry<String, Object> entry:map.entrySet()){
+                System.out.println(entry.getValue().getClass());
+            }
+
+            List<Object>li=toList(jsonObject.getJSONArray("lan"));
+            System.out.println(li.toString());
+            for(int i=0;i<li.size();i++){
+                System.out.println(li.get(i).getClass());
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
